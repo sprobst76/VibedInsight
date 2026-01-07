f5a4adefb84e12be7ac19616aa8374e93f09a40c
Stefan Probst-Schendzielorz <Stefan.Probst-Schendzielorz@outlook.de>
2026-01-07 13:44:03 +0100

    Fix Gradle: use gradle.properties for JVM target validation

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -19,26 +19,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-subprojects {
-    afterEvaluate {
-        if (project.hasProperty("android")) {
-            project.extensions.configure<com.android.build.gradle.BaseExtension>("android") {
-                compileOptions {
-                    sourceCompatibility = JavaVersion.VERSION_17
-                    targetCompatibility = JavaVersion.VERSION_17
-                }
-            }
-        }
-        if (project.plugins.hasPlugin("org.jetbrains.kotlin.android")) {
-            project.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-                compilerOptions {
-                    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-                }
-            }
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
